644beb23a9c9f801f23b7662ef6a861ea20b55b7
olusegun <olusegunbamgbelu755@gmail.com>
2025-10-21 15:01:37 +0100

    done

---
+++ index.js
@@ -0,0 +1,8 @@
+// HEADER ---- HEADER 
+
+// get the time and date as per requirement, add it to the html document
+const time = document.getElementById('time')
+const date = Date.now()
+time.innerText = date
+
+
